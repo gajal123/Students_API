4a376743cb51d9aa5bf7f2f9aa07993bcd0e5740
Gajal <gajal.agarwala4@gmail.com>
2018-03-12 11:44:27 +0200

    Register working

--- Accounts/views.py
+++ Accounts/views.py
@@ -14,8 +14,8 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            user = auth.authenticate(username=username, password=raw_password)
+            auth.login(request, user)
             return redirect('home')
     else:
         form = UserCreationForm()
